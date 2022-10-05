e23937cbedd4330370ef772b44a714eefb63e761
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-10-05 15:39:38 +0300

    AccountForm.tsx

---
+++ src/AccountForm.tsx
@@ -0,0 +1,36 @@
+import { FormWrapper } from './FormWrapper';
+
+type AccountData = {
+  email: string;
+  password: string;
+};
+
+type AccountFormProps = AccountData & {
+  updateFields: (fields: Partial<AccountData>) => void;
+};
+
+export function AccountForm({
+  email,
+  password,
+  updateFields,
+}: AccountFormProps) {
+  return (
+    <FormWrapper>
+      <label htmlFor="email">Email</label>
+      <input
+        autoFocus
+        required
+        type="email"
+        value={email}
+        onChange={e => updateFields({ email: e.target.value })}
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        id="password"
+        type="password"
+        value={password}
+        onChange={e => updateFields({ password: e.target.value })}
+      />
+    </FormWrapper>
+  );
+}
