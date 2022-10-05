cd2b11390dd40bffff744a1a0deed543ae00f5cb
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-10-05 15:39:30 +0300

    AddressForm.tsx

---
+++ src/AddressForm.tsx
@@ -0,0 +1,54 @@
+import { FormWrapper } from './FormWrapper';
+
+type AddressData = {
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+};
+
+type AddressFormProps = AddressData & {
+  updateFields: (fields: Partial<AddressData>) => void;
+};
+
+export function AddressForm({
+  street,
+  city,
+  state,
+  zip,
+  updateFields,
+}: AddressFormProps) {
+  return (
+    <FormWrapper>
+      <label htmlFor="street">Street</label>
+      <input
+        autoFocus
+        required
+        type="text"
+        value={street}
+        onChange={e => updateFields({ street: e.target.value })}
+      />
+      <label htmlFor="city">City</label>
+      <input
+        required
+        type="text"
+        value={city}
+        onChange={e => updateFields({ city: e.target.value })}
+      />
+      <label htmlFor="state">State</label>
+      <input
+        required
+        type="text"
+        value={state}
+        onChange={e => updateFields({ state: e.target.value })}
+      />
+      <label htmlFor="zip">Zip</label>
+      <input
+        required
+        type="text"
+        value={zip}
+        onChange={e => updateFields({ zip: e.target.value })}
+      />
+    </FormWrapper>
+  );
+}
